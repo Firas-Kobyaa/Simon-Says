adec205743935cd9c32c93e9299151dbd7abd676
Firas-Kobyaa <firaskobyaa@outlook.com>
2024-06-30 00:26:02 +0300

    placed all js code in jquery's ready function

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,45 @@
-const buttonColors = ["red", "blue", "green", "yellow"]
-let gamePattern = [];
-let userClickedPattern = []
-let started = false
-let level = 0
+$(document).ready(  function() {
+    
+    const buttonColors = ["red", "blue", "green", "yellow"];
+    let gamePattern = [];
+    let userClickedPattern = [];
+    let started = false;
+    let level = 0;
+    
+    document.addEventListener('keypress', () => {
+    
+        if (!started) {
+            document.querySelector("#level-title").textContent = `Level ${level}`;
+            nextSequence();
+            started = true;
+        }
+    });
 
-document.addEventListener('keypress', () => {
 
-    if (!started) {
-        document.querySelector("#level-title").textContent = `Level ${level}`
-        nextSequence();
-        started = true
-    }
-})
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
