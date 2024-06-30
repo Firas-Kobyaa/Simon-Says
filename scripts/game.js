f9c312e655311859f8ace0b168c5a4f9788d1f38
Firas-Kobyaa <firaskobyaa@outlook.com>
2024-06-30 18:04:05 +0300

    did some cleaning

--- scripts/game.js
+++ scripts/game.js
@@ -1,45 +1,15 @@
-$(document).ready(  function() {
-    
-    const buttonColors = ["red", "blue", "green", "yellow"];
-    let gamePattern = [];
-    let userClickedPattern = [];
-    let started = false;
-    let level = 0;
-    
-    document.addEventListener('keypress', () => {
-    
-        if (!started) {
-            document.querySelector("#level-title").textContent = `Level ${level}`;
-            nextSequence();
-            started = true;
-        }
-    });
+// Arrays for patterns of clicks
+let player_clicks = [];
+let required_clicks = [];
 
+// boolean for serves as an indicator for startig the game
+let gameStarted = false;
 
+// To keep track of button clicks and which level we're on
+let click_count = 0;
+let level_count = 1;
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+// Vars for DOM elements
+const document_body = document.getElementsByTagName("body")[0];
+const level_title = document.getElementById("level-title");
+const game_buttons = document.querySelectorAll(".btn");
