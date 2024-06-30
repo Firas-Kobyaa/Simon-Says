028bbeaa6dee9ac28d37eb77fc500e9b1d7f31da
Firas-Kobyaa <firaskobyaa@outlook.com>
2024-06-30 18:04:42 +0300

    added audio funcction

--- scripts/game.js
+++ scripts/game.js
@@ -13,3 +13,20 @@ let level_count = 1;
 const document_body = document.getElementsByTagName("body")[0];
 const level_title = document.getElementById("level-title");
 const game_buttons = document.querySelectorAll(".btn");
+
+// Audio function
+const playAudio = (selectedButton) => {
+  let audio;
+
+  if (selectedButton.classList.contains("green")) {
+    audio = new Audio("./sounds/green.mp3");
+  } else if (selectedButton.classList.contains("red")) {
+    audio = new Audio("./sounds/red.mp3");
+  } else if (selectedButton.classList.contains("yellow")) {
+    audio = new Audio("./sounds/yellow.mp3");
+  } else if (selectedButton.classList.contains("blue")) {
+    audio = new Audio("./sounds/blue.mp3");
+  }
+  return audio;
+};
+
